2d4ded51433673a3afaba1dfb4c306e39e0b5bd8
Sebastienlumuna <sebastienlumuna8@gmail.com>
2025-07-10 00:08:34 +0100

    modification du carroussel

--- js/main.js
+++ js/main.js
@@ -74,20 +74,15 @@
         time: 2000
     });
 
-
-    // Header carousel
-    $(".header-carousel").owlCarousel({
-        autoplay: false,
-        smartSpeed: 1500,
-        items: 1,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ]
-    });
+$(".header-carousel").owlCarousel({
+    autoplay: true, // active le défilement automatique
+    autoplayTimeout: 5000, // délai en ms entre chaque slide (modifiable)
+    smartSpeed: 1500,
+    items: 1,
+    dots: false,
+    loop: true,
+    nav: false // retire les boutons de navigation
+});
 
 
     // Testimonials carousel
